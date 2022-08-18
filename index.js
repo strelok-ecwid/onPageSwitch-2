ab5c90231978cffedb83ac3999c6a01da68b1913
strelok <strelok@ecwid.com>
2022-08-18 14:40:53 +0400

    1.3

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 Ecwid.OnAPILoaded.add(function() {
-    console.log("Ecwid storefront JS API has loaded - 104204 - 1.2");
+    console.log("Ecwid storefront JS API has loaded - 104204 - 1.3");
 });
 Ecwid.OnPageSwitch.add(function(page) {
     if (page.type === "PRODUCT") {
-        console.log(page.name)
+        console.log(page.nameTransated)
         
     } else if (page.type === "CATEGORY") {
-        console.log(page.name)
+        console.log(page.nameTranslated)
         
     }
 })
 Ecwid.OnPageLoaded.add(function(page) {
     if (page.type == "CATEGORY") {
-      console.log(page.name)
+      console.log(page.nameTranslated)
   }
 });
